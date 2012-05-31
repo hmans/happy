27ab9c0520eb165a210e34a083792c50e8e93b8a
Hendrik Mans <hendrik@mans.de>
2012-05-31 21:27:12 +0200

    Add Happy::Utils::AppSpawner

---
+++ lib/happy/utils/app_spawner.rb
@@ -0,0 +1,33 @@
+module Happy
+  module Utils
+    class AppSpawner
+      attr_reader :options
+
+      def initialize(options = {})
+        @options = {
+          :scripts => './app/*.rb'
+        }.merge(options)
+      end
+
+      def call(env)
+        app.call(env)
+      end
+
+      def app
+        @app = reload_app? ? load_app : @app
+      end
+
+      def load_app
+        Happy::Controller.build.tap do |klass|
+          Dir[options[:scripts]].each do |file|
+            klass.instance_eval File.read(file)
+          end
+        end
+      end
+
+      def reload_app?
+        !Happy.env.production?
+      end
+    end
+  end
+end
