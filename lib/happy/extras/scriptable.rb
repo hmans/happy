9529c6120b967e1042dfacb45234a97063d56ba7
Hendrik Mans <hendrik@mans.de>
2012-06-07 22:11:29 +0200

    Remove AppSpawner and replace it with Happy::Extras::Scriptable

---
+++ lib/happy/extras/scriptable.rb
@@ -0,0 +1,37 @@
+require 'happy/extras/permissions'
+
+module Happy
+  module Extras
+    class Scriptable < Happy::Controller
+      def route
+        Happy::Context.class_eval(load_script 'context.rb')
+        run_script 'permissions.rb'
+        run_script 'route.rb'
+      end
+
+      def run_script(name)
+        instance_exec &get_proc_for_script(name)
+      end
+
+      def get_proc_for_script(name)
+        if reload_script?(name)
+          procs[name] = eval "lambda { %s }" % load_script(name)
+        end
+        procs[name]
+      end
+
+      def reload_script?(name)
+        !procs[name] || Happy.env.development?
+      end
+
+      def load_script(name)
+        file_name = File.expand_path(File.join(options[:directory], name))
+        File.read(file_name)
+      end
+
+      def procs
+        @procs ||= {}
+      end
+    end
+  end
+end
