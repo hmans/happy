b322c149312f1e270b45df30326a2aee09a1d8c7
Hendrik Mans <hendrik@mans.de>
2012-06-12 17:58:12 +0200

    Documentation.

--- lib/happy/controller/helpers.rb
+++ lib/happy/controller/helpers.rb
@@ -2,9 +2,16 @@ require 'happy-helpers'
 
 module Happy
   class Controller
+    # A collection of useful helper methods.
+    #
     module Helpers
+      # Load a whole bunch of helpers fromi HappyHelpers. This includes stuff
+      # like url_for, link_to and more.
       include HappyHelpers::Helpers
 
+      # Renders "something". This method takes a closer look at what this
+      # "something" is and then dispatches to a more specific method.
+      #
       def render(what, options = {}, &blk)
         case what
           when NilClass   then ''
@@ -14,11 +21,15 @@ module Happy
         end
       end
 
+      # Render a template from the controller's view folder.
+      #
       def render_template(name, variables = {}, &blk)
         path = options[:views] || './views'
         HappyHelpers::Templates.render(File.join(path, name), self, variables, &blk)
       end
 
+      # Render a resource.
+      #
       def render_resource(resource, options = {})
         # build name strings
         singular_name = resource.class.to_s.tableize.singularize
