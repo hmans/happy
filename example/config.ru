148a41d990e2a94eb358760b71262f2f7b0504bf
Hendrik Mans <hendrik@mans.de>
2012-06-30 18:59:09 +0200

    Update the Happy Tutorial.

--- example/config.ru
+++ example/config.ru
@@ -1,3 +1,30 @@
 require 'happy'
 Dir['./*.rb'].each { |n| require n }
-run HelloWorld
+
+class TutorialMounter < Happy::Controller
+  def route
+    @examples = HappyTutorial.constants
+    serve_examples
+    serve_example_index
+  end
+
+  def serve_examples
+    @examples.each do |example|
+      on example.to_s do
+        run eval("HappyTutorial::#{example}")
+      end
+    end
+  end
+
+  def serve_example_index
+    response.body << html_tag(:h1) { "The Happy Tutorial" }
+    response.body << html_tag(:ul) do
+      @examples.inject("") do |s, example|
+        s << html_tag(:li) { link_to example, example }
+      end
+    end
+    halt!
+  end
+end
+
+run TutorialMounter
--- example/happy-tutorial.rb
+++ example/happy-tutorial.rb
@@ -1,53 +1,133 @@
-# The most important class Happy provides is [Happy::Controller](http://rdoc.info/github/hmans/happy/master/Happy/Controller).
-# Pretty much everything you develop with Happy will be some class derived from it.
-# Happy Controllers are discrete applications that can be mounted as Rack
-# apps or invoked from within other Happy Controllers, but more on that
-# later.
-#
-# Every controller defines a `#route` instance method that is called for
-# every request hitting your application. You can do all sorts of stuff
-# inside that method (first and foremost, it allows you to deal with specific
-# URLs being requested), but more on that later. For now, we'll simply return
-# a string -- which will be returned as the response to the browser.
+# ## Introduction
 
-class HelloWorld < Happy::Controller
-  def route
-    "Hello, world!"
+# Welcome to the **Happy Tutorial**!
+#
+# Happy is a framework that offers a significantly
+# different approach to developing web applications from that of Ruby on Rails,
+# Sinatra and most other Ruby web frameworks available.
+#
+# Let's jump right in!
+#
+module HappyTutorial
+
+  # ### A Note about this tutorial
+  #
+  # This tutorial is actually a fully functional Happy application. Start
+  # it by issuing the following commands inside the `example/` directory:
+  #
+  #     bundle install
+  #     bundle exec rackup
+  #
+  # The application will then be running on [http://localhost:9292](http://localhost:9292).
+
+  # ## Happy Controllers
+
+  # Pretty much everything you develop with Happy will be some class derived from
+  # [Happy::Controller](http://rdoc.info/github/hmans/happy/master/Happy/Controller).
+  # In this tutorial, you will notice that we will be creating a whole bunch of
+  # these classes, wrapping individual lessons.
+  # Happy controllers are discrete applications that can be mounted as Rack
+  # apps or invoked from within other Happy Controllers.
+  #
+  # Unlike controllers in Rails, Happy controllers describe functionality, not
+  # individual resources; they can be very simple (eg. serving a simple string, like above),
+  # or very complex (eg. serving a complete admin backend, blog engine or similar).
+  # Happy leaves it up to you
+  # to modularize your application's functionality into controllers small and big.
+  #
+  # ### The `#route` method
+  #
+  # Every Happy controller defines a `#route` method that is called for
+  # every incoming request. This is a significant departure from how you develop
+  # with, for example, Rails, where every requested URL is mapped to a specific
+  # piece of code through routes defined in a separate configuration file.
+  #
+  # Within the `#route` method, it is then expected that you, somehow, handle the
+  # request. Make it return a string, that string will be returned to the requesting
+  # client as the response. Of course, there's a whole lot more you can do within
+  # that method; but more on that later.
+
+  # Let's start with a
+  # simple "Hello world" style example.
+
+  class HelloWorldExample < Happy::Controller
+    def route
+      "Hello, world!"
+    end
   end
-end
 
 
-# ### Handling URLs
-#
-# Obviously, your app should be able to do a lot more than just display
-# a string of text at its root URL. Fret not, for Happy gives you powerful
-# tools to create pretty much any URL structure you can think of; however,
-# the way it works is **significantly different** from what you may be used
-# from other MVC frameworks like Rails or Padrino.
-#
-# To sum things up, the main difference in Happy is that each and every request
-# that hits your application will go through the root controller's `#route`
-# method, leaving it up to that method what happens next.
-#
-# In practice this means that instead of mapping routes to specific controllers
-# or actions, in Happy requests pass through a linear flow of logic that
-# makes decisions about how to handle the request on the fly.
-#
-# Happy provides the `#on` method that allows you to provide code that
-# is only executed if a matching URL is requested. These can be nested,
-# too; for example, this block of code will handle the `/foo` and `/foo/bar`
-# URLs.
-#
-class MyApp < Happy::Controller
-  def route
-    on 'foo' do
-      on 'bar' do
-        "You requested /foo/bar!"
+  # ### Handling URLs
+
+  # Obviously, your app should be able to do a lot more than just display
+  # a string of text at its root URL. Fret not, for Happy gives you powerful
+  # tools to create pretty much any URL structure you can think of.
+  #
+  # Happy provides the `#on` method that allows you to provide code that
+  # is only executed if a matching URL is requested. These can be nested,
+  # too; for example, this block of code will handle the `/foo` and `/foo/bar`
+  # URLs.
+  #
+  class RoutingExample < Happy::Controller
+    def route
+      on 'foo' do
+        on 'bar' do
+          "You requested foo and bar!"
+        end
+
+        "You requested just foo!"
       end
 
-      "You requested /foo!"
+      "You requested this controller's root URL!"
     end
-
-    "You requested /!"
   end
+
+  # ### Handling HTTP Verbs
+
+  # In addition to `#on`, Happy also provides `#on_get`, `#on_post`, `#on_put` and `#on_delete`,
+  # which work just like `#on`, except they also check for the specified HTTP verb (GET, POST,
+  # PUT or DELETE).
+  #
+  class HttpVerbRoutingExample < Happy::Controller
+    def route
+      on_get('foo') { "You did a GET request on foo!" }
+      on_post('foo') { "You did a POST request on foo!" }
+
+      # Note that the actual path parameter is optional, so this will work as well:
+      on 'bar' do
+        on_get  { "You did a GET request on bar!" }
+        on_post { "You did a POST request on bar!" }
+      end
+
+      # The `#current_url` method will return the current URL plus an optional suffix.
+      # This is great for building relative links within your controller.
+      "Try GET and POST on #{current_url('foo')} and #{current_url('bar')}!"
+    end
+  end
+
+  # ### Dealing with Request Parameters
+
+  # Inside a Happy controller, the `#params` method will allow you to access the
+  # request parameters. In addition, you can use placeholders inside path names
+  # to retrieve parameters from the actual URLs requested.
+  class PathParameterExample < Happy::Controller
+    def route
+      on 'hello' do
+        on :name do
+          "Hello, #{params['name']}!"
+        end
+
+        "You didn't provide a name. :/"
+      end
+
+      # You can even use inline placeholders within a path:
+      on 'hi-:name' do
+        "Hi, #{params['name']}!"
+      end
+
+      "Try #{link_to current_url('hello/joe')} or #{link_to current_url('hi-joe')}!"
+    end
+  end
+
+# The end.
 end
